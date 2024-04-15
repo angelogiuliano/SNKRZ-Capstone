353f2652d32f70a56780b9f4f1a76b4d1c271fc7
ang <angelo03giuliano@gmail.com>
2024-04-15 17:35:08 +0200

    Update CheckoutSuccess.jsx

--- fe/src/components/CheckoutSuccess/CheckoutSuccess.jsx
+++ fe/src/components/CheckoutSuccess/CheckoutSuccess.jsx
@@ -1,5 +1,27 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 export const CheckoutSuccess = () => {
-  return <div className="container">CheckoutSuccess</div>;
+  const [timer, setTimer] = useState(4);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    localStorage.setItem("cart", JSON.stringify([]));
+    const timeoutId = setTimeout(() => {
+      if (timer > 0) {
+        setTimer(timer - 1);
+      } else {
+        navigate("/");
+      }
+    }, 1000);
+
+    return () => clearTimeout(timeoutId)
+  }, [timer]);
+
+  return (
+    <div className="container">
+      <h4 className="text-center">Grazie per il tuo acquisto!</h4>
+      <p className="text-center">Verrai reindirizzato alla pagina home tra {timer} secondi..</p>
+    </div>
+  );
 };
